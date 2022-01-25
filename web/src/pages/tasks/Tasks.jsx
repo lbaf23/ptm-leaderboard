869f5c613823eb85deb0d845e0f5502e061d3856
lbaf23 <2282480289@qq.com>
2022-01-25 19:27:41 +0800

    refactor: modify tasks with Card

--- web/src/pages/tasks/Tasks.jsx
+++ web/src/pages/tasks/Tasks.jsx
@@ -1,4 +1,4 @@
-import {Button, Table} from "antd";
+import {Button, Card, Space} from "antd";
 import React, {useEffect, useState} from "react";
 import {ArrowRightOutlined} from "@ant-design/icons"
 import {Link} from "react-router-dom";
@@ -32,41 +32,29 @@ function Tasks() {
   useEffect(()=>{
     // TODO get task list
     setTasks([
-      {id: '1', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '2', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '3', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '4', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '1', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '2', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '3', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '4', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '1', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '2', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '3', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '4', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '1', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '2', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '3', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '4', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '1', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '2', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '3', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '4', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '1', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '2', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '3', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
-      {id: '4', name: 'T1: Pre-Trained Models are unsafe', metric: 'Accuracy'},
+      {id: '1', title: 'Aspect-Based Sentiment Analysis', metric: 'Accuracy'},
+      {id: '2', title: 'Coreference Resolution', metric: 'Accuracy'},
+      {id: '3', title: 'Dependency Parsing', metric: 'Accuracy'},
+      {id: '4', title: 'Machine Reading Comprehension', metric: 'Accuracy'},
     ])
     setLoading(false);
   },[])
   return (
     <div>
       <p style={{fontSize: '20px', textAlign: 'center', fontWeight: 'bold'}}>Tasks</p>
-      <Table
-        dataSource={tasks}
-        columns={columns}
-        loading={loading}
-      />
+      {tasks.map((item, index) => (
+        <Link to={item.id}>
+          <Card
+            loading={loading}
+            title={item.title}
+            hoverable
+            style={{
+              marginTop: '20px'
+            }}
+          >
+          </Card>
+        </Link>
+      ))}
     </div>
   )
 }
