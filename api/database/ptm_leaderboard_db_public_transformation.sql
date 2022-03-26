468e49e3865e40ea4f17396e5a4cc431a9c6ea80
lbaf23 <2282480289@qq.com>
2022-03-26 20:20:48 +0800

    refactor: update sql

--- api/database/ptm_leaderboard_db_public_transformation.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public.transformation (id, data_set_refer, title, url) VALUES ('tst', 'imdb', 'Task Specific Transformations', null);
-INSERT INTO public.transformation (id, data_set_refer, title, url) VALUES ('ut', 'imdb', 'Universal Transformations', null);
-INSERT INTO public.transformation (id, data_set_refer, title, url) VALUES ('tst', 'yb', 'Task Specific Transformations', null);
