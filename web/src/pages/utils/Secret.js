1738c6a9a3dc34918e8613577e234b6ae7cc3dbd
lbaf23 <56927357+lbaf23@users.noreply.github.com>
2022-04-21 11:59:30 +0800

    feat: use RSA

---
+++ web/src/pages/utils/Secret.js
@@ -0,0 +1,13 @@
+import JSEncrypt from 'jsencrypt'
+
+
+function encode(data) {
+  let encryptor = new JSEncrypt()
+  let pubKey = '-----BEGIN PUBLIC KEY-----MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC1QQRl0HlrVv6kGqhgonD6A9SU6ZJpnEN+Q0blT/ue6Ndt97WRfxtSAs0QoquTreaDtfC4RRX4o+CU6BTuHLUm+eSvxZS9TzbwoYZq7ObbQAZAY+SYDgAA5PHf1wNN20dGMFFgVS/y0ZWvv1UNa2laEz0I8Vmr5ZlzIn88GkmSiQIDAQAB-----END PUBLIC KEY-----'
+  encryptor.setPublicKey(pubKey)
+  return encryptor.encrypt(data)
+}
+
+export default {
+  encode
+}
