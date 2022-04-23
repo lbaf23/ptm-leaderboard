191527bcbb6b23229d9bb4849c07911ac6e38193
lbaf23 <56927357+lbaf23@users.noreply.github.com>
2022-04-23 20:41:08 +0800

    refactor

--- microk8s/deploy.sh
+++
@@ -1,4 +0,0 @@
-docker tag lbaf23/ptm-leaderboard-attack:1.0.0 localhost:32000/ptm-leaderboard-attack:1.0.1
-docker push localhost:32000/ptm-leaderboard-attack:1.0.1
-microk8s kubectl apply -f attack-local.yml
- 
