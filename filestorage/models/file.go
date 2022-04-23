f68c32085df3b7e000b2e2302c21a76a7d2f3fe3
lbaf23 <56927357+lbaf23@users.noreply.github.com>
2022-04-23 23:01:45 +0800

    Delete file.go

--- filestorage/models/file.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type File struct {
-	FileName    string `json:"fileName" gorm:"not null;column:file_name"`
-	FileUrl     string `json:"fileUrl" gorm:"not null;column:file_url"`
-	FileType    string `json:"fileType" gorm:"not null;column:file_type"`
-	StorageType string `json:"storageType" gorm:"not null;column:storage_type"`
-}
