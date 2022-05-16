0a68abb6ae87554051d86291013f7a0315c981c5
lbaf23 <56927357+lbaf23@users.noreply.github.com>
2022-05-16 21:20:59 +0800

    Update base64.sh

--- base64.sh
+++ base64.sh
@@ -1,13 +1,66 @@
-echo -n '' | base64
-echo -n '' | base64
-echo -n '' | base64
-echo -n '' | base64
-echo -n '' | base64
+echo ===============database===============
 
-echo -n '' | base64
-echo -n '' | base64
-echo -n '' | base64
-echo -n '' | base64
-echo -n '' | base64
+echo dbhost:
+echo -n dbhost | base64
 
-echo -n '' | base64
+echo dbname:
+echo -n postgres | base64
+
+echo dbpassword:
+echo -n xxx | base64
+
+echo dbport:
+echo -n 5432 | base64
+
+echo dbname:
+echo -n ptm_leaderboard_db | base64
+
+
+echo ===============casdoor===============
+
+echo casdoorendpoint:
+echo -n http://xxx:30003 | base64
+
+echo casdoorclientid:
+echo -n xxx | base64
+
+echo casdoorclientsecret:
+echo -n xxx | base64
+
+echo casdoororganization:
+echo -n ptm | base64
+
+echo applicationname
+echo -n ptm-leaderboard | base64
+
+
+echo ===============nats===============
+
+echo natsurl
+echo -n nats://xxx@xxx.xxx.xxx.xxx:30005 | base64
+
+
+echo ===============file===============
+echo localserver:
+echo -n http://xxx:30006/file | base64
+ 
+
+echo ===============oss===============
+
+echo region:
+echo -n xxx | base64
+
+echo accesskeyid:
+echo -n xxx | base64
+
+echo accesskeysecret:
+echo -n xxx | base64
+
+echo rolearn:
+echo -n xxx | base64
+
+echo rolesessionname:
+echo -n ptm-session | base64
+
+echo bucket:
+echo -n xxx | base64
